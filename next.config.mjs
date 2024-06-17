aaadd8d730f239cea4c3508a2eb4b5ee2502b543
jobeyobey <hollands.joe@gmail.com>
2024-06-17 23:52:09 +0100

    image fix

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     output: "export",
     reactStrictMode: true,
     basePath: "/portfolio-website",
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
