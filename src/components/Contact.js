000cb82f29f52fc123512c82f411d8f10aa1ccbb
jobeyobey <hollands.joe@gmail.com>
2024-06-17 20:22:47 +0100

    Fix contact title animation

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,40 +3,45 @@ import Image from "next/image";
 
 export default function Contact() {
     return (
-        <section id="contact" className="fade-prep">
-            <SectionTitle title="Contact" subtitle="Let's work together" />
-            <div>
-                <p>Email me at</p>
-                <a id="email" href="mailto: hollands.joe@gmail.com">
-                    hollands.joe@gmail.com
-                </a>
-            </div>
-            <p>Or find me on</p>
-            <div className="social-links">
-                <a
-                    href="https://github.com/Jobeyobey"
-                    target="_blank"
-                    aria-label="Joe Hollands Github"
-                >
-                    <Image
-                        src={`/images/github-light.svg`}
-                        width="20"
-                        height="20"
-                        aria-hidden="true"
-                    />
-                </a>
-                <a
-                    href="https://www.linkedin.com/in/joehollands/"
-                    target="_blank"
-                    aria-label="Joe Hollands LinkedIn"
-                >
-                    <Image
-                        src={`/images/linkedin-blue.svg`}
-                        width="20"
-                        height="20"
-                        aria-hidden="true"
-                    />
-                </a>
+        <section id="contact">
+            <SectionTitle
+                title="Contact"
+                subtitle="Open to job opportunities"
+            />
+            <div className="fade-prep">
+                <div>
+                    <p>Email me at</p>
+                    <a id="email" href="mailto: hollands.joe@gmail.com">
+                        hollands.joe@gmail.com
+                    </a>
+                </div>
+                <p>Or find me on</p>
+                <div className="social-links">
+                    <a
+                        href="https://github.com/Jobeyobey"
+                        target="_blank"
+                        aria-label="Joe Hollands Github"
+                    >
+                        <Image
+                            src={`/images/github-light.svg`}
+                            width="20"
+                            height="20"
+                            aria-hidden="true"
+                        />
+                    </a>
+                    <a
+                        href="https://www.linkedin.com/in/joehollands/"
+                        target="_blank"
+                        aria-label="Joe Hollands LinkedIn"
+                    >
+                        <Image
+                            src={`/images/linkedin-blue.svg`}
+                            width="20"
+                            height="20"
+                            aria-hidden="true"
+                        />
+                    </a>
+                </div>
             </div>
         </section>
     );
