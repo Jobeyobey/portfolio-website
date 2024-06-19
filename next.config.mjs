531b85fb49afbf684399b5add38e24c52af43617
Joe Hollands <117482381+Jobeyobey@users.noreply.github.com>
2024-06-19 23:49:55 +0100

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: "export",
+    reactStrictMode: true,
+    basePath: "",
+    images: {
+        unoptimized: true,
+    },
+};
+
+export default nextConfig;
